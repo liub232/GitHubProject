9ca3ee397e7c5a5ef68caadeedbbfa86199a8673
liub232 <liubecky232@gmail.com>
2024-06-25 09:44:15 -0700

    Initial Commit

---
+++ GitHubProject/GitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProjectApp.swift
+//  GitHubProject
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
